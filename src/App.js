c1c082365b9049c23633dc1272bff57d95ab872c
Navven Raj MN <navvenr96@gmail.com>
2021-01-30 17:07:30 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function App() {
   return (
-    <div >
-      Phone Directory
+    <div>
+      <div className="header">
+        Phone Directory
+        </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
