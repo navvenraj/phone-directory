b96be7bcfaf0c99b0809a812b3d84d0906c43261
Navven Raj MN <navvenr96@gmail.com>
2021-01-31 22:32:42 +0530

    Styled ‘Header’ (Inline Styling Approach-2)

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 const Header = function () {
+
+    const headerStyle = {
+        textAlign: 'center',
+        padding: 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+     };
+
     return (
+        /*
         //inline style Approach 1
         <div style={{ textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' }}>
             Phone Directory
         </div>
+        */
+
+        //inline styling Approach 2
+         <div style={headerStyle}>
+            Phone Directory
+         </div>
     )
 }
 
