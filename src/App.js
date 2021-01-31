40ab201c2d6930a07834ed7df07b252ce115653b
Navven Raj MN <navvenr96@gmail.com>
2021-02-01 00:01:08 +0530

    Renders subscribers dynamically and Adding Delete button

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import './Header.css';
 import './App.css';
 
 function App() {
+
+  let subscribers = [
+    {
+      id:1,
+      name: "Shilpha Baat",
+      phone: "888889899"
+
+
+    },
+    {
+      id:2,
+      name: "Navven Raj",
+      phone: "888889899"
+
+    }
+  ]
+
   return (
-    <div>
+
+    <div className="component-container">
       <Header />
       <div className="component-body-container">
         <button className="custom-btn add-btn">Add</button>
@@ -15,6 +33,19 @@ function App() {
           <span className="grid-item name-heading">Name</span>
           <span className="grid-item phone-heading">Phone</span>
         </div>
+
+        {
+          subscribers.map(sub =>{
+            return <div key={sub.id} className="grid-container">
+            <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+            <span className="grid-item action-btn-container">
+              <button className="custom-btn delete-btn">Delete</button>
+            </span>
+          </div>
+          })
+        }
+
       </div>
     </div>
   );
