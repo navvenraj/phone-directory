0db05c5a010d134c876439cc38147ec4d3995aaa
Navven Raj MN <navvenr96@gmail.com>
2021-01-31 22:27:55 +0530

    Styled ‘Header’ (Inline Styling Approach - 1)

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Header = function() {
+const Header = function () {
     return (
-        <div className="header">
+        //inline style Approach 1
+        <div style={{ textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' }}>
             Phone Directory
         </div>
     )
