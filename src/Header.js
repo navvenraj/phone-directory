78742b6aca55e112effe2932e19ee505a9c99e21
Navven Raj MN <navvenr96@gmail.com>
2021-02-02 13:31:11 +0530

    Passed heading to Header component as props(Class component)

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,47 @@
-import React from 'react';
+//import React from 'react';
+//import './Header.css';
 
-const Header = function (props) {
+//const Header = function (props) {
 
-    /*
-   //inline styling approach 2
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-     };
-     */
+/*
+//inline styling approach 2
+const headerStyle = {
+    textAlign: 'center',
+    padding: 20,
+    background: '#000',
+    color: '#fff',
+    textTransform: 'uppercase'
+ };
+ */
 
-    return (
-        /*
-        //inline style Approach 1
-        <div style={{ textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' }}>
-            Phone Directory
-        </div>
-        */
+//    return (
+/*
+//inline style Approach 1
+<div style={{ textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' }}>
+    Phone Directory
+</div>
+*/
 
-        //inline styling Approach 2
-         <div className="header">
-            {props.heading}
-         </div>
-    )
+//inline styling Approach 2
+//       <div className="header">
+//          {props.heading}
+//       </div>
+//  )
+//}
+
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                {this.props.heading}
+            </div>
+
+        )
+    }
 }
+
 
 export default Header;
